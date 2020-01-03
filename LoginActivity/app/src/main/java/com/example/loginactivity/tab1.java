478eaf3711d84a03a02ba0e2337e13a69a56c63d
Kalogon <ghlee5084@naver.com>
2020-01-03 23:53:59 +0900

    send to server

--- LoginActivity/app/src/main/java/com/example/loginactivity/tab1.java
+++ LoginActivity/app/src/main/java/com/example/loginactivity/tab1.java
@@ -3,9 +3,11 @@ package com.example.loginactivity;
 
 import android.Manifest;
 import android.content.ContentResolver;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.core.app.ActivityCompat;
@@ -18,10 +20,36 @@ import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.FacebookCallback;
+import com.facebook.login.LoginManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import com.facebook.CallbackManager;
+import cz.msebera.android.httpclient.HttpEntity;
+import cz.msebera.android.httpclient.HttpResponse;
+import cz.msebera.android.httpclient.NameValuePair;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.entity.UrlEncodedFormEntity;
+import cz.msebera.android.httpclient.client.methods.HttpPost;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
 
 
 /**
@@ -30,6 +58,7 @@ import java.util.List;
 public class tab1 extends Fragment {
 
     private View v;
+    TextView tvData;
 
 
     public tab1() {
@@ -40,8 +69,9 @@ public class tab1 extends Fragment {
         RecyclerView rv = v.findViewById(R.id.contact);
 
         List<A_DATA> datas = new ArrayList<>();
-
+        JSONObject jsonObject_each = new JSONObject();
         ContentResolver resolver = getContext().getContentResolver();
+        tvData = (TextView)v.findViewById(R.id.textView);
 
         Uri phoneUri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
 
@@ -52,7 +82,8 @@ public class tab1 extends Fragment {
 
 
         Cursor cursor = resolver.query(phoneUri, proj, null, null, null);
-
+        String result="";
+        int temp=0;
         if (cursor != null) {
             while (cursor.moveToNext()) {
                 int id = cursor.getInt(cursor.getColumnIndex(proj[0]));
@@ -62,6 +93,24 @@ public class tab1 extends Fragment {
 
                 A_DATA data = new A_DATA(id, name, tel, image);
                 datas.add(data);
+
+
+
+
+
+                result+=Integer.toString(data.getId())+data.getImage()+data.getName()+data.getTel();
+
+
+
+
+
+
+                temp++;
+            }
+            try {
+                jsonObject_each.accumulate("test",result);
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
         }
         cursor.close();
@@ -70,6 +119,8 @@ public class tab1 extends Fragment {
 
         rv.setAdapter(my_adapter);
         rv.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        new JSONTask(jsonObject_each).execute("http://192.249.19.254:7180/post");
     }
 
     @Override
@@ -77,6 +128,7 @@ public class tab1 extends Fragment {
                              Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         v = inflater.inflate(R.layout.fragment_tab1, container, false);
+
 
         ArrayList<String> permissions = new ArrayList<>();
         if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
@@ -110,4 +162,88 @@ public class tab1 extends Fragment {
         }
     }
 
+
+    public class JSONTask extends AsyncTask<String, String, String>{
+        JSONObject jsonObject_each = new JSONObject();
+        public JSONTask(JSONObject jsonObject_each){
+            this.jsonObject_each=jsonObject_each;
+        }
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                //JSONObject를 만들고 key value 형식으로 값을 저장해준다.
+
+                HttpURLConnection con = null;
+                BufferedReader reader = null;
+
+                try{
+                    URL url = new URL(urls[0]);
+                    //연결을 함
+                    con = (HttpURLConnection) url.openConnection();
+
+                    con.setRequestMethod("POST");//POST방식으로 보냄
+                    con.setRequestProperty("Cache-Control", "no-cache");//캐시 설정
+                    con.setRequestProperty("Content-Type", "application/json");//application JSON 형식으로 전송
+
+
+                    con.setRequestProperty("Accept", "text/html");//서버에 response 데이터를 html로 받음
+                    con.setDoOutput(true);//Outstream으로 post 데이터를 넘겨주겠다는 의미
+                    con.setDoInput(true);//Inputstream으로 서버로부터 응답을 받겠다는 의미
+                    con.connect();
+
+                    //서버로 보내기위해서 스트림 만듬
+                    OutputStream outStream = con.getOutputStream();
+                    //버퍼를 생성하고 넣음
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outStream));
+                    writer.write(jsonObject_each.toString());
+                    writer.flush();
+                    writer.close();//버퍼를 받아줌r
+
+                    //서버로 부터 데이터를 받음
+                    InputStream stream = con.getInputStream();
+
+                    reader = new BufferedReader(new InputStreamReader(stream));
+
+                    StringBuffer buffer = new StringBuffer();
+
+                    String line = "";
+                    while((line = reader.readLine()) != null){
+                        buffer.append(line);
+                    }
+
+                    return buffer.toString();//서버로 부터 받은 값을 리턴해줌 아마 OK!!가 들어올것임
+
+                } catch (MalformedURLException e){
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if(con != null){
+                        con.disconnect();
+                    }
+                    try {
+                        if(reader != null){
+                            reader.close();//버퍼를 닫아줌
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            tvData.setText(result);//서버로 부터 받은 값을 출력해주는 부
+        }
+    }
+
 }
+
+
+
